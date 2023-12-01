6e0c46c35f6d8d1f596dde9bcd11055175798dc2
BaharOssareh <78626041+BaharOssareh@users.noreply.github.com>
2023-12-01 17:39:01 -0500

    small

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
     let amount = amountEl.value;
     let measure = measureEl.value;
 
-    if (!foodText || !amount || !measureEl) {
+    if (!food || !amount || !measureEl) {
       alertEl.textContent = "Please fill all mandatory fields!";
       modal.style.display = "block";
       return;
